3d17810d2b555ee6a83f3cedca305980f5d6a04e
mathewcmartin <chactasstryker@gmail.com>
2017-05-24 14:17:15 -0700

    First commit. Updated constring and refactored .get request.

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -11,12 +11,8 @@ var app = app || {};
     //       Don't forget to remove the headers from our request - we're no longer using a token on the
     //       client side of our app, our new proxyGitHub function will be handling the token using our
     //       new environment variable!
-    $.ajax({
-      url: `https://api.github.com/user/repos`,
-      type: 'GET',
-      headers: {'Authorization': `token ${githubToken}`}
-    })
-    .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+    $.get('/github/user/repos')
+      .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
   };
 
